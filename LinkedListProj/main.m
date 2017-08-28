ba33138ba7ba8c1a372bda4c4aee511e608c03c5
Nate <nschrein@nmu.edu>
2017-08-28 10:37:59 -0400

    Initial Commit

---
+++ LinkedListProj/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LinkedListProj
+//
+//  Created by Nate Schreiner on 8/28/17.
+//  Copyright © 2017 Nate Schreiner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
